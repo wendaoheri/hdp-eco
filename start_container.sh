a9f3084979f39249739080dd46b6b7a385341736
wendaoheri <wendaoheri@gmail.com>
2018-11-27 23:12:22 +0800

    change hdfs web ui port to 5007

--- start_container.sh
+++ start_container.sh
@@ -1,1 +1,1 @@
-docker run --rm -it --link mysql --privileged -p 10000:10000/tcp -p 1099:1099/tcp -p 1992:1992/tcp -p 19999:19999/tcp -p 22:22/tcp -p 30000:30000/tcp -p 50070:50070/tcp -p 8088:8088/tcp -p 9000:9000/tcp -p 9083:9083/tcp registry.cn-hangzhou.aliyuncs.com/hdp-eco/hdp-eco:latest
+docker run --rm -it --name hdp-eco --link mysql --privileged -p 10000:10000/tcp -p 1099:1099/tcp -p 1992:1992/tcp -p 19999:19999/tcp -p 22:22/tcp -p 30000:30000/tcp -p 5007:50070/tcp -p 8088:8088/tcp -p 9000:9000/tcp -p 9083:9083/tcp registry.cn-hangzhou.aliyuncs.com/hdp-eco/hdp-eco:latest
