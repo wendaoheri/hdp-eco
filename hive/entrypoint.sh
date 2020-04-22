7814d69a99310f6d0c1cb87e561afc1db86ccebb
liuxiang466 <liuxiang466@pingan.com.cn>
2020-04-22 17:40:49 +0800

    add wait-for-it script

--- hive/entrypoint.sh
+++ hive/entrypoint.sh
@@ -3,11 +3,11 @@ CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup --"
-    su -c "schematool -dbType mysql -initSchema" hive
-    su -c "hdfs dfs -mkdir /tmp" hive
-    su -c "hdfs dfs -mkdir /user/hive/warehouse" hive
-    su -c "hdfs dfs -chmod g+w /tmp" hive
-    su -c "hdfs dfs -chmod g+w /user/hive/warehouse" hive
+    su -c "/wait-for-it.sh mysql:3306 --timeout=30 --strict -- schematool -dbType mysql -initSchema" hive
+    su -c "/wait-for-it.sh hadoop:8020 --timeout=30 --strict -- hdfs dfs -mkdir /tmp" hive
+    su -c "/wait-for-it.sh hadoop:8020 --timeout=30 --strict -- hdfs dfs -mkdir /user/hive/warehouse" hive
+    su -c "/wait-for-it.sh hadoop:8020 --timeout=30 --strict -- hdfs dfs -chmod g+w /tmp" hive
+    su -c "/wait-for-it.sh hadoop:8020 --timeout=30 --strict -- hdfs dfs -chmod g+w /user/hive/warehouse" hive
 else
     echo "-- Not first container startup --"
 fi
