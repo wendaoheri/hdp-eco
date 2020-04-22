337655eeeff8ee2d26f9fcdc8a086bf453e39988
liuxiang466 <liuxiang466@pingan.com.cn>
2020-04-22 18:11:16 +0800

    create hive hdfs warehouse dir

--- hive/entrypoint.sh
+++ hive/entrypoint.sh
@@ -5,7 +5,7 @@ if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     echo "-- First container startup --"
     su -c "/wait-for-it.sh mysql:3306 --timeout=30 --strict -- schematool -dbType mysql -initSchema" hive
     su -c "/wait-for-it.sh hadoop:8020 --timeout=30 --strict -- hdfs dfs -mkdir /tmp" hive
-    su -c "/wait-for-it.sh hadoop:8020 --timeout=30 --strict -- hdfs dfs -mkdir /user/hive/warehouse" hive
+    su -c "/wait-for-it.sh hadoop:8020 --timeout=30 --strict -- hdfs dfs -mkdir -p /user/hive/warehouse" hive
     su -c "/wait-for-it.sh hadoop:8020 --timeout=30 --strict -- hdfs dfs -chmod g+w /tmp" hive
     su -c "/wait-for-it.sh hadoop:8020 --timeout=30 --strict -- hdfs dfs -chmod g+w /user/hive/warehouse" hive
 else
