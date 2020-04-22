b9656bd3644d30279496cebf962cb526fb5bd5cf
liuxiang466 <liuxiang466@pingan.com.cn>
2020-04-22 16:42:15 +0800

    supervisor Centralized

--- hive/entrypoint.sh
+++ hive/entrypoint.sh
@@ -1,18 +1,15 @@
 #/bin/bash
-
-su hive
 CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup --"
-    schematool -dbType mysql -initSchema
-    hdfs dfs -mkdir         /tmp
-    hdfs dfs -mkdir         /user/hive/warehouse
-    hdfs dfs -chmod g+w     /tmp
-    hdfs dfs -chmod g+w     /user/hive/warehouse
+    su -c "schematool -dbType mysql -initSchema" hive
+    su -c "hdfs dfs -mkdir /tmp" hive
+    su -c "hdfs dfs -mkdir /user/hive/warehouse" hive
+    su -c "hdfs dfs -chmod g+w /tmp" hive
+    su -c "hdfs dfs -chmod g+w /user/hive/warehouse" hive
 else
     echo "-- Not first container startup --"
 fi
-exit
 
 supervisord -c /etc/supervisord.conf
