8904a93efe281d0cbbf7e4dccd519c8355e72176
Sean Liu <wendaoheri@gmail.com>
2020-07-06 16:20:24 +0800

    add kylin config

---
+++ kylin/entrypoint.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
+    touch $CONTAINER_ALREADY_STARTED
+    echo "-- First container startup --"
+    su -c "/wait-for-it.sh hadoop:8020 --timeout=30 --strict -- hdfs dfs -mkdir -p /kylin" kylin
+    su -c "/wait-for-it.sh hadoop:8020 --timeout=30 --strict -- hdfs dfs -chmod -R 775 /kylin" kylin
+else
+    echo "-- Not first container startup --"
+fi
+
+supervisord -c /etc/supervisord.conf
